88060080aca7e5471232e87580d4e2b4b3eab15d
Enzo Vergara <enzoamb123@gmail.com>
2020-10-30 23:34:40 -0300

    fix regex get src parameter

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function getPartialTags(html) {
 
   var partial_regex = "<\s*partial[^>]*>(.*?)<\s*/\s*partial>";
   var partial_regex_self_closing = "<\s*partial[^>]*/>";
-  var src_regex = /src\=([^\s]*)\s/;
+  var src_regex = /src\=([^\s]*)\s?/;
   var parameters_regex = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|\s*\/?[>"']))+.)["']?/g;
   var tags = [...html.matchAll(partial_regex)];
   var tags_self_closing = [...html.matchAll(partial_regex_self_closing)];
